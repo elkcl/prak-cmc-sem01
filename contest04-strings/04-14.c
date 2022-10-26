02562c0e2994c9dc1c893f6a26dc55f76810daf8
Elk Cloner <28754537+elkcl@users.noreply.github.com>
2022-10-26 11:57:44 +0300

    Ещё немного задач во 4 контесте

--- contest04-strings/04-14.c
+++ contest04-strings/04-14.c
@@ -12,6 +12,24 @@ int main(void) {
     memset(suf_even, 0, n * sizeof(int));
     memset(suf_odd, 0, n * sizeof(int));
     for (int i = n - 2; i >= 0; --i) {
-
+        suf_even[i] = suf_even[i + 1];
+        suf_odd[i] = suf_odd[i + 1];
+        if (s[i + 1] == 'A') {
+            if (i % 2 == 0)
+                ++suf_even[i];
+            else
+                ++suf_odd[i];
+        }
+    }
+    int curr_even = 0, curr_odd = 0;
+    for (int i = 0; i < n; ++i) {
+        if (curr_even + suf_even[i] == curr_odd + suf_odd[i])
+            printf("%d ", i + 1);
+        if (s[i] == 'A') {
+            if (i % 2 == 0)
+                ++curr_even;
+            else
+                ++curr_odd;
+        }
     }
 }
---
+++ contest04-strings/04-17.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXN 2000003
+
+char s[MAXN];
+
+int main(void) {
+
+}
